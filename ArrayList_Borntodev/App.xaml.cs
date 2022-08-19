a56292f8459a35dcc4925b7fa255d465a81fdc95
chawanitvir <chawanitvir@gmail.com>
2022-08-20 02:49:38 +0700

    Add project files.

---
+++ ArrayList_Borntodev/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArrayList_Borntodev
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ArrayList_Borntodev/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Windows;
+
+namespace ArrayList_Borntodev
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ArrayList arr = new ArrayList();
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void AddButton_click(object sender, RoutedEventArgs e)
+        {
+            /* ArrayList Add
+             * 
+             * 
+            arr.Add(txtInput.Text);
+            txtInput.Text = "";
+
+            */
+
+            //Add-Hashtable
+            hashtable.Add(txtKey.Text,txtValue.Text);
+        }
+
+        private void RemoveButton_click(object sender, RoutedEventArgs e)
+        {
+            /*ArrayList Remove
+             * 
+             * 
+            arr.Remove(txtInput.Text);
+            txtInput.Text = "";
+
+            */
+
+            //Remove-Hashtable
+            hashtable.Remove(txtKey.Text);
+        }
+
+        private void ShowAllButton_click(object sender, RoutedEventArgs e)
+        {
+            /*ArrayList
+            //Show all
+            foreach(object data in arr)
+            {
+                MessageBox.Show(data.ToString());
+            }*/
+
+            ICollection icollection = hashtable.Keys;
+            //ShowAll-Hashtable
+            foreach (string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
